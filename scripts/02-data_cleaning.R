1bfda48e8f2758499fd2ef365e962bb84c477469
Sandy Yu <jingying.yu@mail.utoronto.ca>
2024-09-17 14:46:24 -0400

    commiting the subway_delay_code cleaning code chunk

--- scripts/02-data_cleaning.R
+++ scripts/02-data_cleaning.R
@@ -18,9 +18,11 @@ library(readr)
 bus_delay_2023 <- read_excel("inputs/data/downloaded_xlsx_format/bus_delay_2023.xlsx")
 streetcar_delay_2023 <- read_excel("inputs/data/downloaded_xlsx_format/streetcar_delay_2023.xlsx")
 subway_delay_2023 <- read_excel("inputs/data/downloaded_xlsx_format/subway_delay_2023.xlsx")
-
+subway_delay_code <- read_excel("inputs/data/downloaded_xlsx_format/subway_delay_code.xlsx")
 
 #### Clean data ####
+
+###Delay Data ###
 bus_delay <-
   bus_delay_2023 |>
   janitor::clean_names() |>
@@ -38,7 +40,25 @@ subway_delay <-
   rename(location = station)
 
 
+### Subway Delay Code ###
+#clean name of columns
+subway_delay_code <- subway_delay_code |>
+  janitor::clean_names() |>
+  select(-1, -4, -5)
 
+#take non-null values of columns. 6 and 7 and create a temporary dataset
+temp_data <- subway_delay_code |>
+  filter(!is.na(subway_delay_code[[3]]), !is.na(subway_delay_code[[4]])) %>%  # Filter out rows where either column 3 or 4 is NA
+  select(3, 4) |>
+  rename(sub_rmenu_code = colnames(subway_delay_code)[3],  # Rename column 3
+         code_description_3 = colnames(subway_delay_code)[4])
+
+#select only column 1 and 2 for subway_delay_code
+subway_delay_code <- subway_delay_code |>
+  select(1, 2) |> rename(code = sub_rmenu_code, code_description = code_description_3)
+
+#rbind the two datasets
+subway_delay_code <- rbind(subway_delay_code, temp_data)
 
 #### Save data ####
 write_csv(bus_delay, "inputs/data/cleaned_data/bus_delay.csv")
@@ -46,4 +66,6 @@ write_csv(bus_delay, "inputs/data/cleaned_data/bus_delay.csv")
 write_csv(streetcar_delay, "inputs/data/cleaned_data/streetcar_delay.csv")
 
 write_csv(subway_delay, "inputs/data/cleaned_data/subway_delay.csv")
+
+write_csv(subway_delay_code, "inputs/data/cleaned_data/subway_delay_code.csv")
 
